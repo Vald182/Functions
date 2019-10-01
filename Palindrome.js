eecad4f6c0698359bb3242f3407ace0ed6667343
Vald182 <55404228+Vald182@users.noreply.github.com>
2019-10-01 10:05:04 +0300

    Add files via upload

---
+++ Palindrome.js
@@ -0,0 +1,14 @@
+const isPalindrome = (str) => {
+  let nstr = String(str);
+  if (nstr.length < 2) {
+    return true;
+  }
+  else if (nstr[0] === nstr[nstr.length - 1]){
+    nstr = nstr.substr(0,1);
+    nstr = nstr.substr(-1, 1);
+    return isPalindrome(nstr);
+  }
+  else {
+    return false;
+  }
+}
