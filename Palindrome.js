47503815188c6bb92eda04ca99c8f05dd9ad5235
Vald182 <55404228+Vald182@users.noreply.github.com>
2019-10-01 11:44:46 +0300

    Update Palindrome.js

--- Palindrome.js
+++ Palindrome.js
@@ -1,16 +1,20 @@
 const isPalindrome = (str) => {
   let nstr = String(str);
   nstr = nstr.toUpperCase();
+  
+  
   if (nstr.length < 2) {
     return true;
   }
+  
+  
   else if (nstr[0] === nstr[nstr.length - 1]){
     nstr = nstr.substr(0,1);
     nstr = nstr.substr(-1, 1);
     return isPalindrome(nstr);
   }
-  else {
-    return false;
+  return false;
   }
 }
+
 export default isPalindrome;
