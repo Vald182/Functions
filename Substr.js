f7180d5ca46e123b85d781581d2f545ca5cacaeb
Vald182 <55404228+Vald182@users.noreply.github.com>
2019-10-04 21:39:07 +0300

    Update Substr.js

--- Substr.js
+++ Substr.js
@@ -1,42 +1,34 @@
-const substr = (str, ind, len) => {
-  if (ind === undefined && len === undefined){
+const substr = (str, ind = 0, len = 0) => {
+  if (ind === 0 && len === 0) {
     return str; 
-  }
-
+    }
+  
+  const nstr = String(str);
   let nlen = len;
   let nind = ind;
-  const nstr = String(str);
-  
-  if (ind === undefined){
-    nind = 0;
-  }
-  
-  if (len === undefined){
-    nlen = 0;
-  }
 
-  if (nlen === 0 || ind > (nstr.length - 1)){
+  if (nlen === 0 || ind > (nstr.length - 1)) {
     return '';
-  }
-
+    }
+  
   if (len < 0){
     nlen = 1;
-  }
+    }
 
-  if ((ind + len - 1) > nstr.length){
+  if ((ind + len - 1) > nstr.length) {
     nlen = nstr.length;
-  }
+    }
 
   if (ind < 0){
     nind = 0;
-  }
+    }
 
   let result = '';
 
-  for (let i = nind; i < (nlen + nind); i += 1){
+  for (let i = nind; i < (nlen + nind); i += 1) {
     result = result + nstr[i];
-  }
+    }
   return result;
-}
+  }
 
 export default substr;
