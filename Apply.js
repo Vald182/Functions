b9ed49b3365c030a730b6a42ca1e8d4cd0a306dd
Vald182 <55404228+Vald182@users.noreply.github.com>
2019-10-04 18:19:31 +0300

    Update Apply.js

--- Apply.js
+++ Apply.js
@@ -1,15 +1,15 @@
-const apply = (num = 0, func = n => n, arg) =>{
+const apply = (num = 0, func = n => n, arg) => {
   
-  if (arg === undefined){
+  if (arg === undefined) {
     throw new Error('Argument is not found');
-  }  
+    }  
   
   let res = arg;
 
-  for (let i = 0; i < num; i += 1){
+  for (let i = 0; i < num; i += 1) {
     res = func(res);
-  }
+    }
   return res;
-}
+  }
 
 export default apply;
