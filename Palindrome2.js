706e461db8d1f0dfc4711ab12b5b3eaeba15c743
Vald182 <55404228+Vald182@users.noreply.github.com>
2019-10-05 14:01:23 +0300

    Update Palindrome2.js

--- Palindrome2.js
+++ Palindrome2.js
@@ -1,17 +1,17 @@
 const isPalindrome = (str) => {
-  
+
   const nstr = String(str);
   let res = '';
-  
+
   for (let i = nstr.length; i > 0; i -= 1) {
     res = res + nstr[i - 1];
-  } 
+  }
 
   if (res === nstr) {
     return true;
   }
 
   return false;
-  }
+}
 
-  export default isPalindrome;
+export default isPalindrome;
